bf6192386a2700256bf3cf63ae4ed532d6f6e087
Antoine Mouly <amouly@k0r4p7.42mulhouse.fr>
2023-05-15 14:43:03 +0200

    travail en cours

--- src/player.c
+++ src/player.c
@@ -6,7 +6,7 @@
 /*   By: amouly <amouly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:06:29 by llion             #+#    #+#             */
-/*   Updated: 2023/05/15 13:44:52 by amouly           ###   ########.fr       */
+/*   Updated: 2023/05/15 14:33:56 by amouly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_player	*init_player(t_cub *c, int i, int j)
 	player->img = mlx_new_image(c->mlx, c->tilesize, c->tilesize);
 	player->x_pos = i;
 	player->y_pos = j;
-	player->ang = 0.6;
+	player->ang = 0;
 	if (c->map[i][j] == 'N')
 		player->ang = PI / 2;
 	else if (c->map[i][j] == 'S')
